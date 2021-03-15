e6463957682a016314a3d8c3cbd165d93521a858
Fernando Almeida <lfalmeida@me.com>
2021-03-15 20:56:01 -0300

    adding express validator

--- auth/src/routes/signUp/index.ts
+++ auth/src/routes/signUp/index.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import validationRules from './validation';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', validationRules, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
   res.send('hello');
 });
 
