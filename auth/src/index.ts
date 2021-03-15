7dc8a2e08af3a750d7a9ca55784f6b4ad7c5caff
Fernando Almeida <lfalmeida@me.com>
2021-03-15 19:31:57 -0300

    adding nginx ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/test', (req, res) => {
+  res.send('hello');
+});
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000.')
+  console.log('Listening on port 3000.');
 });
