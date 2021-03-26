4cdafbca03b12d8ac5a66cd2562ec7c274870216
Fernando Almeida <lfalmeida@me.com>
2021-03-26 15:26:04 -0300

    adding signout component

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -17,7 +17,7 @@ const useRequest = ({ url, method, body, onSuccess }) => {
         <div className="alert alert-danger">
           <h4>Ooops...</h4>
           <ul className="my-0">
-            {err.response.data.errors.map(err => <li key={err.message}>{err.message}</li>)}
+            {err.response && err.response.data.errors.map(err => <li key={err.message}>{err.message}</li>)}
           </ul>
         </div>
       );
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
-export default App;
+
+AppComponent.getInitialProps = async appContext => {
+  const httpClient = buildClient(appContext.ctx);
+
+  let pageProps;
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  try {
+    const { data } = await httpClient.get('/api/users/current');
+    return {
+      pageProps,
+      ...data
+    }
+  } catch (error) {
+    return {
+      pageProps,
+      currentUser: null
+    };
+  }
+};
+
+export default AppComponent;
