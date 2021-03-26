d960bc3aef64401934a214d26869b80ff74c5858
Fernando Almeida <lfalmeida@me.com>
2021-03-26 20:19:27 -0300

    writing a test for creating ticket route

---
+++ tickets/src/routes/createTicket/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post('/api/tickets', (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+export { router as createTicketRouter }
+
--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -2,16 +2,16 @@ import express from 'express';
 import 'express-async-errors';
 
 import { NotFoundError } from '@blackcoffee/common';
-
+import { createTicketRouter } from './createTicket';
 
 const supportedRoutes = [
-
+  createTicketRouter
 ];
 
 const loadRoutes = (app: express.Application) => {
-  // supportedRoutes.map(route => {
-  //   app.use(route);
-  // });
+  supportedRoutes.map(route => {
+    app.use(route);
+  });
 
   app.all('*', async () => {
     throw new NotFoundError();
