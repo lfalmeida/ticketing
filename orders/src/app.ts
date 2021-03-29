152f1687979ba3a8281f02c8a990091e056aa7a1
Fernando Almeida <lfalmeida@me.com>
2021-03-29 14:46:31 -0300

    creating orders service structure

---
+++ orders/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from '@blackcoffee/common';
+import loadRoutes from './routes/index';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}));
+
+loadRoutes(app);
+
+app.use(errorHandler);
+
+export { app }
