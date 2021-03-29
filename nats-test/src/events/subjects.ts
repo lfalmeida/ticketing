970704c88bdb9f706ba3745f27e603b206061811
Fernando Almeida <lfalmeida@me.com>
2021-03-29 16:07:46 -0300

    removing unused files

--- nats-test/src/events/subjects.ts
+++
@@ -1,5 +0,0 @@
-export enum Subjects {
-  TicketCreated = 'ticket:created',
-  OrderUpdated = 'order:updated'
-}
-
--- nats-test/src/events/ticketCreatedPublisher.ts
+++
@@ -1,7 +0,0 @@
-import { BasePublisher } from './basePublisher';
-import { ITicketCreatedEvent } from './ticketCreatedEvent';
-import { Subjects } from './subjects';
-
-export class TicketCreatedPublisher extends BasePublisher<ITicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
