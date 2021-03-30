33182c504663c38ac0b16d710f211a45b8b681a3
Fernando Almeida <lfalmeida@me.com>
2021-03-29 22:40:42 -0300

    adding tests

--- orders/src/routes/deleteOrder/__test__/index.test.ts
+++ orders/src/routes/deleteOrder/__test__/index.test.ts
@@ -2,6 +2,7 @@ import { OrderStatus } from '@blackcoffee/common';
 import request from 'supertest';
 import { app } from '../../../app';
 import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats';
 import { OrderService } from '../../../services/orders';
 
 it('marks an order as cancelled', async () => {
@@ -34,4 +35,29 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('Emits a order cancelled event');
+it('emits a order cancelled event', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "Concert",
+    price: 30
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // create request to create order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // create request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
