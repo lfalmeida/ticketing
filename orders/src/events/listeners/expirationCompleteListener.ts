4ae476e44872f9c3b21febd8fbf9b0ea927763cb
Fernando Almeida <lfalmeida@me.com>
2021-03-30 20:08:19 -0300

    expirationCompleteListener orders service

---
+++ orders/src/events/listeners/expirationCompleteListener.ts
@@ -0,0 +1,30 @@
+import { BaseListener, IExpirationCompleteEvent, OrderStatus, Subjects } from "@blackcoffee/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/orderCancelledPublisher";
+import { queueGroupName } from "./queueGroupName";
+
+export class ExpirationCompleteListener extends BaseListener<IExpirationCompleteEvent> {
+  readonly subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: IExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+    if (!order) {
+      throw new Error('Order not found.');
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id
+      }
+    });
+
+    msg.ack();
+  }
+}
