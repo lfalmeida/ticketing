e4bf3478b3d219aad612a5c39a093cd8c40066a5
Fernando Almeida <lfalmeida@me.com>
2021-03-30 20:54:02 -0300

    adding payments serice structure

---
+++ payments/src/routes/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'express-async-errors';
+
+import { NotFoundError } from '@blackcoffee/common';
+
+const supportedRoutes = [
+
+];
+
+const loadRoutes = (app: express.Application) => {
+  // supportedRoutes.map(route => {
+  //   app.use(route);
+  // });
+
+  app.all('*', async () => {
+    throw new NotFoundError();
+  })
+};
+
+export default loadRoutes;
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,59 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { UserPayload } from '@blackcoffee/common';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string
+    }
+  }
+}
+
+jest.mock('../nats');
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'test_key';
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // build a JWT payload { id, email }
+  const payload: UserPayload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'fake@email.com'
+  };
+  // create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  // build session object { jwt: MY_JWT }
+  const session = { token };
+  // turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  // take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  // return a string thats the cookie with ecoded data
+  return `express:sess=${base64}`;
+};
